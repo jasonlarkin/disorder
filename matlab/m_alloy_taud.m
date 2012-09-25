168a63e0a4c2551f3247f127725d53e9c63624a9
jasonlarkin <jasonlarkin84@gmail.com>
2012-09-25 11:06:53 -0400

    added nmd vs ald

--- matlab/m_alloy_taud.m
+++ matlab/m_alloy_taud.m
@@ -1,17 +1,17 @@
 
-str.NMD = '/home/jason/disorder/lj/alloy/10K/0.5/10x/NMD/1/work/';
+str.NMD = '/home/jason/disorder2/lj/alloy/10K/0.15/10x/nmd_vc/work/1/';
 SED=load(strcat(str.NMD,'SEDdata.mat'));
 NMD=load(strcat(str.NMD,'NMDdata.mat'));
 
 SED = nmd_convert_data(NMD,SED);
 
-ALLOY = m_ld_defect_life(NMD,SED, 0.5 , 1 , 3 , 2.0 );
+ALLOY = m_ld_defect_life(NMD,SED, 0.15 , 1 , 3 , 1.3 );
 
 %SHOULD THE EIGENVECTORS/FREQS FROM FROM VC, OR PERFECT M=1?
 
 
 loglog(...
-    SED.freq*NMD.LJ.tau,SED.life/NMD.LJ.tau,'.',...
+    SED.freq,SED.life,'.',...
     ALLOY.freq, ALLOY.life,'.',...
     ALLOY.freq, 1E4*ALLOY.freq.^(-4)...
     )
