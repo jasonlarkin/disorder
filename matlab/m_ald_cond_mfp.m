b60f0174b35b6c1a2b21d6c581188428ee3a2124
jasonlarkin <jasonlarkin84@gmail.com>
2012-10-09 03:23:09 -0400

    added presentation

---
+++ matlab/m_ald_cond_mfp.m
@@ -0,0 +1,31 @@
+function [freq_bins cond_freq] = m_ald_cond_freq(freq,life,groupvel,VOLUME,NUM_BINS)
+%--------------------------------------------------------------------------
+%cond = m_ald_cond_freq(freq,life,groupvel,VOLUME)
+%assumes quantities in mks units, not lj
+%--------------------------------------------------------------------------
+
+constant = m_constant; lj = m_lj;
+
+freq_bins = linspace( 0 , max(freq) , NUM_BINS); 
+
+%freq_bins
+
+for ibin = 1:length(freq_bins)
+    
+    I = find( freq > freq_bins(ibin) & freq < freq_bins(ibin)+freq_bins(2) );
+    
+%     life(I)
+%     groupvel(I)
+    
+    cond_freq(ibin) =... 
+        sum((constant.kb/VOLUME)*sum( life(I).*(groupvel(I).^2) ) );
+    
+%     pause
+    
+end
+
+
+end
+
+
+
