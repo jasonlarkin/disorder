74d8f7dc5031c06975cd7599a7ad8bcba38dd8d0
jasonlarkin <jasonlarkin84@gmail.com>
2012-10-27 17:57:01 -0400

    added dos

--- matlab/m_dos.m
+++ matlab/m_dos.m
@@ -1,14 +1,37 @@
-function [DOSX DOSY] = m_dos(freq, DOS_BIN, NUM_ATOMS)
+function [dosx dosy] = m_dos(freq, NUM_BINS , broaden)
 %--------------------------------------------------------------------------
 %[DOSX DOSY] = m_dos(freq, DOS_BIN, NUM_ATOMS)
 %--------------------------------------------------------------------------
 
-I = find( freq~=0);
+constant = m_constant; lj = m_lj;
 
-size(I)
+% freq_bins = linspace( 0 , max(freq) , NUM_BINS); 
+% [I,J] = sort(freq); freq_sorted = freq(J);
+% 
+% freq_sorted(1:10)
+% dw_avg =...
+%     real(mean(freq_sorted(2:length(freq))-freq_sorted(1:length(freq)-1)));
+% dw_avg
+% freq_bins
+% for ibin = 1:length(freq_bins)
+% %Lorentzian
+% delwij = ...
+%     freq_sorted - freq_bins(ibin) ;
+% lor = (1.0/pi)*(dw_avg*broaden./( delwij.^2 + dw_avg^2 ) );
+% dosy(ibin) = sum(lor.*freq_sorted);
+% dosx(ibin) = freq_bins(ibin);
+% end
+% dosy = dosy/sum(dosy);
 
-[DOSY DOSX]= hist(freq(I),ceil(NUM_ATOMS/DOS_BIN));
-DOSY = DOSY/sum(DOSY);
+% %remove 0-freq
+% I = find( freq~=0);
+% size(I)
+% [dosy dosx]= hist(freq(I),NUM_BINS);
+% dosy = dosy/sum(dosy);
+
+%remove 0-freq
+[dosy dosx]= hist(freq,NUM_BINS);
+dosy = dosy/sum(dosy);
 
 end
 
