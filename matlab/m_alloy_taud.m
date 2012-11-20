546b4f6a0e85c933dfe59bc1f26d9dd67c326510
jasonlarkin <jasonlarkin84@gmail.com>
2012-11-20 15:36:41 -0500

    added diff plots

--- matlab/m_alloy_taud.m
+++ matlab/m_alloy_taud.m
@@ -1,37 +1,37 @@
 clear
 
-% %--------------------------------------------------------------------------
-% %old
-% %--------------------------------------------------------------------------
-% str.NMD = '/home/jason/disorder2/lj/alloy/10K/0.05/10x/nmd_vc/work/1/';
-% NMD=load(strcat(str.NMD,'NMDdata.mat'));
-% str.save = '/home/jason/disorder2/lj/alloy/10K/0.05/10x/nmd_vc/work/1/';
-% 
-% m1 = 1; m2 = 3; c = 0.05; vm = (1-c)*m1 + c*m2
-% 
-% ALLOY =...
-%     m_ld_defect_life_old(...
-%     NMD,...
-%     c , m1 , m2 , vm , 100 , 0.45 , 1);
-% %--------------------------------------------------------------------------
-
-
 %--------------------------------------------------------------------------
-%new
+%old
 %--------------------------------------------------------------------------
-str.NMD = '//home/jason/disorder2/lj/alloy/40K/0.5/14x/';
-NMD=load(strcat(str.NMD,'nmd.mat'));
-str.save = '/home/jason/disorder2/lj/alloy/40K/0.5/14x/';
+str.NMD = '/home/jason/disorder2/lj/alloy/10K/0.05/10x/NMD/1/work/';
+NMD=load(strcat(str.NMD,'NMDdata.mat'));
+str.save = '/home/jason/disorder2/lj/alloy/10K/0.05/10x/NMD/1/work/';
 
-m1 = 1; m2 = 3; c = 0.5; vm = (1-c)*m1 + c*m2
-tic
+m1 = 1; m2 = 3; c = 0.05; vm = (1-c)*m1 + c*m2
+
 ALLOY =...
-    m_ld_defect_life(...
-    NMD.freq,NMD.eigvec,NMD.NUM_MODES,NMD.NUM_KPTS,...
-    NMD.x0.Nx,NMD.x0.Ny,NMD.x0.Nz,...
-    c , m1 , m2 , vm , 100 , 0.45 , 1);
-toc
+    m_ld_defect_life_old(...
+    NMD,...
+    c , m1 , m2 , vm , 100 , 0.75 , 1);
 %--------------------------------------------------------------------------
+
+
+% %--------------------------------------------------------------------------
+% %new
+% %--------------------------------------------------------------------------
+% str.NMD = '/home/jason/disorder2/lj/alloy/10K/0.05/10x/NMD/1/work/';
+% NMD=load(strcat(str.NMD,'nmd.mat'));
+% str.save = '/home/jason/disorder2/lj/alloy/10K/0.05/10x/NMD/1/work/';
+% 
+% m1 = 1; m2 = 3; c = 0.5; vm = (1-c)*m1 + c*m2
+% tic
+% ALLOY =...
+%     m_ld_defect_life(...
+%     NMD.freq,NMD.eigvec,NMD.NUM_MODES,NMD.NUM_KPTS,...
+%     NMD.x0.Nx,NMD.x0.Ny,NMD.x0.Nz,...
+%     c , m1 , m2 , vm , 100 , 0.45 , 1);
+% toc
+% %--------------------------------------------------------------------------
 
 
 loglog(...
