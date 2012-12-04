d8cd08176321411683f8c2853fc1f3b89a200bfa
jasonlarkin <jasonlarkin84@gmail.com>
2012-12-04 14:25:42 -0500

    added DSF lifetimes

--- matlab/m_dsf.m
+++ matlab/m_dsf.m
@@ -1,26 +1,20 @@
 clear
-
-%AF.str = '/home/jason/disorder2/lj/alloy/10K/0.05/10x/work/';
-
-AF.str = '/home/jason/disorder2/lj/amor/10x/work/';
-%100
-AF.kpt(:,1) = [0.1 0.2 0.3 0.4 0.5]'; 
-AF.kpt(:,2) = 0;
-AF.kpt(:,3) = 0;
-
-
 lj = m_lj; constant = m_constant;
-T = 10*(constant.kb/lj.eps);
+str_af = '/home/jason/disorder2/lj/alloy/10K/0.5/10x/work/';
 
-NMD.Nx = 10; NMD.Ny = 10; NMD.Nz = 10; 
+AF.eigvec = load(strcat(str_af,'AF_eigvec_1.dat'));
+AF.freq = load(strcat(str_af,'AF_freq_1.dat'));
+AF.x0 = m_x0_read(str_af,'x0.data');
 
-AF.eigvec = load(strcat(AF.str,'AF_eigvec_1.dat'));
-AF.freq = load(strcat(AF.str,'AF_freq_1.dat'));
-AF.x0 = m_x0_read(AF.str,'x0.data');
+str_nmd = '/home/jason/disorder2/lj/alloy/10K/0.5/10x/NMD/1/work/';
+nmd = load([str_nmd 'NMDdata.mat']);
+sed = load([str_nmd 'SEDdata.mat']);
 
-NMD.alat = 1.5636;
-%NMD.alat = AF.x0.Lx;
-
+%full
+% AF.kpt(:,1) = nmd.kptmaster(:,1); 
+% AF.kpt(:,2) = nmd.kptmaster(:,2);
+% AF.kpt(:,3) = nmd.kptmaster(:,3);
+%AK.kpt = [AF.kpt(:,1)/nmd.Nx AF.kpt(:,2)/nmd.Ny AF.kpt(:,3)/nmd.Nz]
 %100
 % AF.kpt(:,1) = [0.1 0.2 0.3 0.4 0.5]'; 
 % AF.kpt(:,2) = 0;
@@ -30,36 +24,39 @@ NMD.alat = 1.5636;
 % AF.kpt(:,2) = [0.1 0.2 0.3 0.4 0.5]';
 % AF.kpt(:,3) = 0;
 %111
-% AF.kpt(:,1) = [0.1 0.2 0.3 0.4 0.5]'; 
-% AF.kpt(:,2) = [0.1 0.2 0.3 0.4 0.5]';
-% AF.kpt(:,3) = [0.1 0.2 0.3 0.4 0.5]';
+AF.kpt(:,1) = [0.1 0.2 0.3 0.4 0.5]'; 
+AF.kpt(:,2) = [0.1 0.2 0.3 0.4 0.5]';
+AF.kpt(:,3) = [0.1 0.2 0.3 0.4 0.5]';
 
-% DSF =...
-%     m_dsf_long(...
-%     AF.str , AF.kpt , AF.x0 , AF.freq, AF.eigvec, NMD.alat , 5.0 , 1.1 );
+tic
+DSF =...
+    m_dsf_long(...
+    str_af , [AF.kpt(:,1) AF.kpt(:,2) AF.kpt(:,3)], ...
+    AF.x0 , AF.freq, AF.eigvec, nmd.alat , 20.0 , 1.1 );
+toc
 
+% for ikpt = 1:length(DSF.kpt)
+%     AF.kpt(ikpt,:)
+% semilogy(...
+%     DSF.freq_range,DSF.SL(:,ikpt),...
+%     DSF.freq_range,DSF.SL(:,ikpt),...
+%     DSF.freq_range,DSF.SL(:,ikpt)...
+%     )
+% pause
+% end
+
+tic
 DSF =...
     m_dsf_tran(...
-    AF.str , AF.kpt , AF.x0 , AF.freq, AF.eigvec, NMD.alat , 5.0 , 1.1 );
+    str_af , [AF.kpt(:,1) AF.kpt(:,2) AF.kpt(:,3)], ...
+    AF.x0 , AF.freq, AF.eigvec, nmd.alat , 20.0 , 1.1);
+toc
 
-semilogx(...
-    DSF.freq_range , DSF.EpL(:,1)/max(DSF.EpL(:,1)),...
-    DSF.freq_range , DSF.EpL(:,3)/max(DSF.EpL(:,3)),...
-    DSF.freq_range , DSF.EpL(:,5)/max(DSF.EpL(:,5))...
-    )
-%--------------------------------------------------------------------------
-pause
-%--------------------------------------------------------------------------
-semilogy(...
-    DSF.freq_range,DSF.SL(:,1),...
-    DSF.freq_range,DSF.SL(:,3),...
-    DSF.freq_range,DSF.SL(:,5)...
-    )
-
-
-
-
-
-
-
+% for ikpt = 1:length(DSF.kpt)
+%     AF.kpt(ikpt,:)
+% semilogy(...
+%     DSF.freq_range,DSF.SL(:,ikpt)...
+%     )
+% pause
+% end
 
