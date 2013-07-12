e271f34e5f24f36b62a087507b33726c46bff454
jasonlarkin <jasonlarkin84@gmail.com>
2013-07-12 16:18:59 -0400

    added latest mfp draft

--- matlab/m_pcbm_c60_pcbm28.m
+++ matlab/m_pcbm_c60_pcbm28.m
@@ -1,37 +1,69 @@
 clear
-
+con = m_constant;
 
 str='/home/jason/disorder/pcbm/pcbm_pdb/isolated/c60/';
-
 c60.old.freq = load([str '/oldtersoff/freq.gout']); 
-
 c60.new.freq = load([str 'freq.gout']);
 
-[c60.old.dosx c60.old.dosy] = m_dos(c60.old.freq,10,1,1); 
-[c60.new.dosx c60.new.dosy] = m_dos(c60.new.freq,10,1,1);
+str='/home/jason/disorder/pcbm/allres/';
+c60.dft.freq = load([str 'c60_vgauss']);
+%c60.dft.freq(:,2) = c60.dft.freq(:,2)*con.c*2*pi; 
 
-subplot(2,1,1),...
-    plot(c60.old.freq,c60.old.freq,c60.old.freq,c60.new.freq,'.')
-subplot(2,1,2),...
-    plot(c60.old.dosx , c60.old.dosy , c60.new.dosx , c60.new.dosy)
+[c60.old.dosx c60.old.dosy] = m_dos(c60.old.freq(7:end),15,1,1); 
+[c60.new.dosx c60.new.dosy] = m_dos(c60.new.freq(7:end),15,1,1);
+[c60.dft.dosx c60.dft.dosy] = m_dos(c60.dft.freq(:,2),15,1,1);
+
+% subplot(2,1,1),...
+%     plot(...
+%     c60.old.freq,c60.old.freq,c60.old.freq,c60.new.freq,'.')
+% subplot(2,1,2),...
+    plot(c60.old.dosx , c60.old.dosy , c60.new.dosx , c60.new.dosy,...
+    c60.dft.dosx,c60.dft.dosy)
 pause
 clf
 
 str='/home/jason/disorder/pcbm/pcbm_pdb/isolated/pcbm28/';
 pcbm28.freq = load([str '/freq.gout']); 
-[pcbm28.dosx pcbm28.dosy] = m_dos(pcbm28.freq,5,1,1);
+[pcbm28.dosx pcbm28.dosy] = m_dos(pcbm28.freq(7:end),15,1,1);
+
+str='/home/jason/disorder/pcbm/allres/';
+pcbm28.dft.freq = load([str '/pcbm28_vgauss']); 
+[pcbm28.dft.dosx pcbm28.dft.dosy] = m_dos(pcbm28.dft.freq(:,2),15,1,1);
+
+clf
+plot(pcbm28.dosx , pcbm28.dosy , pcbm28.dft.dosx , pcbm28.dft.dosy)
+pause
+
+str='/home/jason/disorder/pcbm/allres/';
+pcbm.dft.freq = load([str 'pbcm_vgauss']); 
+[pcbm.dft.dosx pcbm.dft.dosy] = m_dos(pcbm.dft.freq(:,2),15,1,1);
 
 str='/home/jason/disorder/pcbm/pcbm_pdb/isolated/';
 pcbm.freq = load([str '/freq.gout']); 
-[pcbm.dosx pcbm.dosy] = m_dos(pcbm.freq,5,1,1);
+[pcbm.dosx pcbm.dosy] = m_dos(pcbm.freq(7:end),15,1,1);
 
 clf
+plot(pcbm.dosx , pcbm.dosy , pcbm.dft.dosx , pcbm.dft.dosy)
+pause
+
+clf
+subplot(2,1,1),...
 plot(...
     c60.new.dosx , c60.new.dosy,...
     pcbm28.dosx , pcbm28.dosy,...
     pcbm.dosx , pcbm.dosy...
     )
+axis([0 3500 0 0.5])
+subplot(2,1,2),...
+plot(...
+    c60.dft.dosx , c60.dft.dosy,...
+    pcbm28.dft.dosx , pcbm28.dft.dosy,...
+    pcbm.dft.dosx , pcbm.dft.dosy...
+    )
+axis([0 3500 0 0.5])
 pause
+
+
 %Di
 pcbm.Di(1,1).Di =...
     m_gulp_af_si_readDi('/home/jason/disorder/pcbm/pcbm_pdb/p1d1_1a1b1c/highT50000/','Di_b0.01.gout');
